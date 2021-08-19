da5e9555d54d8e8edcbcb8dc77895d9d698a194c
Matthias Schmidt <83017351+matthias-schmidt@users.noreply.github.com>
2021-08-19 02:45:34 +0200

    Add files via upload

---
+++ sudoku solver.py
@@ -0,0 +1,250 @@
+from datetime import datetime
+import tracemalloc
+
+from math import factorial,log2
+
+# keeping sets of admissibles, necessary only for advanced sudoku
+
+from itertools import combinations
+from copy import deepcopy
+
+def reduce(s, a):
+    lena, returncode = 0, 0
+    while len(a) != lena:
+        lena = len(a)
+        u = sorted(list(a.keys()))
+        for idx in u:
+                a[idx]=(a[idx]
+                        -set(s[idx[0]])
+                        -set([s[i][idx[1]] for i in range(9)])
+                        -set([s[3*(idx[0]//3)+i][3*(idx[1]//3)+j]
+                              for i in range(3) for j in range(3)]))
+                if len(a[idx]) == 0:
+                    returncode = 1
+                    break
+                if len(a[idx])==1:
+                    s[idx[0]][idx[1]]=list(a[idx])[0]    
+                    a.pop(idx)
+        if returncode == 1: break
+    return (s, a, returncode)
+    
+def decouple(s, a, u):
+    for j in range(2,len(u)):
+        for c in combinations(u,j):
+            cu = set()
+            for index in c:
+                cu = cu|a[index]
+            if len(cu) == j:
+                for index in u-set(c):
+                    a[index] = a[index]-cu
+    return (s, a)
+
+def validator(s):
+    if len(s) != 9: raise TypeError 
+    if set(len(line) for line in s) != {9}: raise TypeError 
+    if set(i for line in s for i in line)-set(range(10)) != set(): raise TypeError 
+    if len([i for line in s for i in line if i != 0]) < 17: raise TypeError
+    for i in range(9):
+        if len(set(s[idx[0]][idx[1]] for idx in lines[i])) != len(list(s[idx[0]][idx[1]] for idx in lines[i])): raise TypeError
+        if len(set(s[idx[0]][idx[1]] for idx in columns[i])) != len(list(s[idx[0]][idx[1]] for idx in columns[i])): raise TypeError
+        if len(set(s[idx[0]][idx[1]] for idx in squares[i])) != len(list(s[idx[0]][idx[1]] for idx in squares[i])): raise TypeError
+    
+    
+            
+# producing all solutions
+
+def sudokusolver1(sudoku):
+
+    s = deepcopy(sudoku)
+    lines = [{(i,j) for j in range(9)} for i in range(9)]
+    columns = [{(i,j) for i in range(9)} for j in range(9)]
+    squares = [{(3*i+k,3*j+l) for k in range(3) for l in range(3)}
+                for i in range(3) for j in range(3)]
+
+    # validation
+    if len(s) != 9: raise TypeError 
+    if set(len(line) for line in s) != {9}: raise TypeError 
+    if set(i for line in s for i in line)-set(range(10)) != set(): raise TypeError 
+    if len([i for line in s for i in line if i != 0]) < 17: raise TypeError
+    for i in range(9):
+        if len(set(s[idx[0]][idx[1]] for idx in lines[i] if s[idx[0]][idx[1]] != 0)) != len(list(s[idx[0]][idx[1]] for idx in lines[i] if s[idx[0]][idx[1]] != 0)): raise TypeError
+        if len(set(s[idx[0]][idx[1]] for idx in columns[i] if s[idx[0]][idx[1]] != 0)) != len(list(s[idx[0]][idx[1]] for idx in columns[i] if s[idx[0]][idx[1]] != 0)): raise TypeError
+        if len(set(s[idx[0]][idx[1]] for idx in squares[i] if s[idx[0]][idx[1]] != 0)) != len(list(s[idx[0]][idx[1]] for idx in squares[i] if s[idx[0]][idx[1]] != 0)): raise TypeError
+
+    # stacks
+    S, C, res = [], [], []
+
+    while True:
+
+        # assign a set of candidates to every indeterminate
+        a = {(i,j):set(range(1,10)) for j in range(9) for i in range(9) if s[i][j]==0}
+        lena = 0
+        while lena != sum([len(value) for value in a.values()]):
+            lena = sum([len(value) for value in a.values()])
+
+            # iteratively reduce candidate sets to admissible values and plug in if appropriate
+            (s,a,returncode) = reduce(s,a)
+
+            # check whether a solution has been found
+            if returncode == 1:
+                try:
+                    while len(C) != 0 and len(C[-1][1]) == 0: S, C = S[:-1], C[:-1]
+                    s = S[-1]
+                    s[C[-1][0][0]][C[-1][0][1]] = C[-1][1][0]
+                    C[-1][1] = C[-1][1][1:]
+                except:
+                    if len(res) != 0: return res
+                    else: raise TypeError
+                break
+            elif len(a) == 0:
+                if len(res) != 0: raise TypeError
+                res = deepcopy(s)
+                try:
+                    while len(C) != 0 and len(C[-1][1]) == 0: S, C = S[:-1], C[:-1]
+                    s = S[-1]
+                    s[C[-1][0][0]][C[-1][0][1]] = C[-1][1][0]
+                    C[-1][1] = C[-1][1][1:]
+                except: return res
+                returncode = 1
+                break
+
+            # decouple candidate sets
+            for i in range(9):
+                (s,a) = decouple(s,a,set(a.keys())&lines[i])
+                (s,a) = decouple(s,a,set(a.keys())&columns[i])
+                (s,a) = decouple(s,a,set(a.keys())&squares[i])
+
+        # backtracking
+        if returncode == 0:
+            S.append(deepcopy(s))
+            m = 10
+            for key in a.keys():
+                if len(a[key]) < m: m, k, c = len(a[key]), key, sorted(a[key])
+            s[k[0]][k[1]] = c[0]
+            C.append([k,c[1:]])
+
+
+
+
+
+# uses only reduce and decouple for solving:
+            
+def sudokusolver2(sudoku):
+    s = deepcopy(sudoku)
+    if (len(s) != 9
+        or set(len(line) for line in s) != {9}
+        or set(i for line in s for i in line)-set(range(10)) != set()
+        or len([i for line in s for i in line if i != 0]) < 17): raise TypeError 
+    lines = [{(i,j) for j in range(9)} for i in range(9)]
+    columns = [{(i,j) for i in range(9)} for j in range(9)]
+    squares = [{(3*i+k,3*j+l) for k in range(3) for l in range(3)}
+                for i in range(3) for j in range(3)]
+    lena = 0
+    a = {(i,j):set(range(1,10)) for j in range(9) for i in range(9) if s[i][j]==0}
+    while lena != sum([len(value) for value in a.values()]):
+        lena = sum([len(value) for value in a.values()])
+        (s,a,returncode) = reduce(s,a)
+        if returncode == 1: return "unsolvable"
+        for i in range(9):
+            (s,a) = decouple(s,a,set(a.keys())&lines[i])
+            (s,a) = decouple(s,a,set(a.keys())&columns[i])
+            (s,a) = decouple(s,a,set(a.keys())&squares[i])
+    if len(a) == 0: return s
+    return "No reduce and decouple solution"
+
+            
+        
+
+
+
+
+
+
+
+arg = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+    ]
+
+
+    
+
+
+arg =    [[5,3,0,0,7,0,0,0,0],
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
+
+arg = [
+    [0,0,0,3,2,0,5,0,0],
+    [3,7,0,0,5,0,0,0,8],
+    [0,0,0,0,0,0,6,0,0],
+    [0,0,0,8,0,0,0,1,0],
+    [0,0,0,0,0,2,0,0,0],
+    [0,3,1,0,0,0,0,4,7],
+    [9,0,0,0,6,0,0,0,5],
+    [1,0,2,0,0,0,9,0,0],
+    [0,0,0,5,0,0,0,0,0],
+    ]
+
+arg = [
+    [1,2,3,4,5,6,7,8,9],
+    [4,5,6,0,0,0,0,0,0],
+    [7,8,9,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+    ]
+
+arg = [[0, 5, 7, 2, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 9, 0, 8, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 9, 4],
+[8, 0, 0, 0, 0, 0, 0, 3, 0],
+[0, 0, 2, 0, 0, 7, 0, 0, 0],
+[9, 0, 0, 0, 3, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 7],
+[0, 8, 0, 0, 0, 0, 0, 0, 5]]
+
+arg = [[1, 1, 3, 4, 5, 6, 7, 8, 9], [4, 0, 6, 7, 8, 9, 1, 2, 3], [7, 8, 9, 1, 2, 3, 4, 5, 6], [2, 3, 4, 5, 6, 7, 8, 9, 1], [5, 6, 7, 8, 9, 1, 2, 3, 4], [8, 9, 1, 2, 3, 4, 5, 6, 7], [3, 4, 5, 6, 7, 8, 9, 1, 2], [6, 7, 8, 9, 1, 2, 3, 4, 5], [9, 1, 2, 3, 4, 5, 6, 7, 8]]
+
+arg = [[0, 8, 0, 0, 0, 9, 7, 4, 3], [0, 5, 0, 0, 0, 8, 0, 1, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0], [8, 0, 0, 0, 0, 5, 0, 0, 0], [0, 0, 0, 8, 0, 4, 0, 0, 0], [0, 0, 0, 3, 0, 0, 0, 0, 6], [0, 0, 0, 0, 0, 0, 0, 7, 0], [0, 3, 0, 5, 0, 0, 0, 8, 0], [9, 7, 2, 4, 0, 0, 0, 5, 0]]
+
+arg = [[6, 0, 0, 0, 0, 3, 0, 0, 0], [0, 0, 0, 4, 0, 8, 0, 0, 0], [0, 0, 0, 5, 0, 0, 0, 0, 8], [0, 5, 0, 0, 0, 4, 2, 7, 9], [0, 8, 0, 0, 0, 5, 0, 3, 0], [0, 7, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 1, 0, 8, 0, 0, 0, 5, 0], [3, 4, 7, 9, 0, 0, 0, 8, 0]]
+
+arg =    [[5,3,0,0,7,0,0,0,0],
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
+
+
+
+starttime=datetime.now()
+#tracemalloc.start()
+x = sorted(sudokusolver1(arg))
+for line in x:
+    print(line)
+#print(tracemalloc.get_traced_memory())
+#tracemalloc.stop()
+print(format(datetime.now()-starttime))
+
---
+++ sudoku validator.py
@@ -0,0 +1,104 @@
+from datetime import datetime
+import tracemalloc
+
+from math import factorial,log2
+
+class Sudoku(object):
+
+    def __init__(self, data):       # can contain standard methods applicable to objects via object.method
+        self.board = data[:]
+        self.dim = len(self.board)
+        self.dimroot = int((self.dim)**.5)
+        self.range = set(range(1,self.dim+1))
+
+    def is_valid(self):     # definition of special method applicable to object via object.method()
+
+        N, n = self.dim, self.dimroot
+        if n**2 != N: return False
+
+        s = self.board
+        if N == 1: return s[0][0] is 1
+        for line in s:
+            if len(line) != N: return False
+
+        c = self.range
+        for i in range(N):
+            if set(s[i]) != c: return False
+            if set([s[j][i] for j in range(N)]) != c: return False
+            if set([s[n*(i//n)+j][n*(i%n)+k] for j in range(n) for k in range(n)]) != c: return False
+
+        return True
+    
+
+arg = [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+  [6, 7, 2, 1, 9, 0, 3, 4, 8],
+  [1, 0, 0, 3, 4, 2, 5, 6, 0],
+  [8, 5, 9, 7, 6, 1, 0, 2, 0],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 0, 1, 5, 3, 7, 2, 1, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 0, 0, 4, 8, 1, 1, 7, 9]
+  ]
+
+
+arg = [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+
+arg = Sudoku([[1,4, 2,3],
+      [3,2, 4,1],
+      [4,1, 3,2],
+      [2,3, 1,4]
+      ])
+
+arg = Sudoku(
+    [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [2, 3, 1, 5, 6, 4, 8, 9, 7],
+    [3, 1, 2, 6, 4, 5, 9, 7, 8],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [5, 6, 4, 8, 9, 7, 2, 3, 1],
+    [6, 4, 5, 9, 7, 8, 3, 1, 2],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [8, 9, 7, 2, 3, 1, 5, 6, 4],
+    [9, 7, 8, 3, 1, 2, 6, 4, 5]
+    ])
+
+arg = Sudoku([[True]])
+
+arg = Sudoku([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]])
+
+
+arg = Sudoku([[1,4, 2,3],
+      [3,2, 4,1],
+      [4,1, 3,2],
+      [2,3, 1,4]
+      ])
+
+starttime=datetime.now()
+#tracemalloc.start()
+print(arg.is_valid())
+#print(tracemalloc.get_traced_memory())
+#tracemalloc.stop()
+print(format(datetime.now()-starttime))
+
+
+
